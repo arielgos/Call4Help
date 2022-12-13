3a71779d28e38a324df6eebc86f8ebf60d5dabcb
Ariel Ortuno Solis <arielsolis@sherpacrm.com>
2022-12-13 13:53:22 -0400

    Adding CI Script

---
+++ deploy_ci.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# file: deploy_ci.sh
+echo "Running update..."
+echo "Cleaning..."
+rm -Rfv Call4Help-master
+rm -Rfv master.zip
+
+echo "Downloading..."
+curl -L https://github.com/arielgos/Call4Help/archive/refs/heads/master.zip -o master.zip
+
+echo "Uncompressing..."
+unzip master.zip 
+
+echo "Updating files..."
+cd Call4Help-master
+cp -R * /var/www/html/
+cd ..
+echo "Update finish..."
